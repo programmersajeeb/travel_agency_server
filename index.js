30ab1fb2f32489fad7504c96a021426be081f682
Programmer Sajeeb <sajeeb.programmer@gmail.com>
2022-01-27 20:38:00 +0600

    some update new

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ async function run() {
         const database = client.db('travel_agency');
         const blogStore = database.collection('blogs');
         const newBlogStore = database.collection('newBlogs');
+        const usersCollection = database.collection('users');
 
         // GET API ALL BLOGS
         app.get('/blogs', async(req,res)=>{  
@@ -96,6 +97,36 @@ async function run() {
         const result = await newBlogStore.deleteOne(query);
         res.json(result);
     })
+
+
+    // =================================================
+
+    app.get('/users/:email', async (req, res)=>{
+        const email = req.params.email;
+        const query = {email: email};
+        const user = await usersCollection.findOne(query);
+        let isAdmin = false;
+        if(user?.role === 'admin'){
+          isAdmin = true;
+        }
+        res.json({admin: isAdmin});
+      })
+    
+      // Firebase to database save data post api
+      app.post('/users', async (req, res)=>{
+        const newUser = req.body;
+        const result = await usersCollection.insertOne(newUser);
+        res.json(result);
+      });
+    
+      // Make Admin
+      app.put('/users/admin', async(req, res) =>{
+        const user = req.body;
+            const filter = {email: user.email};
+            const updateDoc = {$set: {role: 'admin'}};
+            const result = await usersCollection.updateOne(filter, updateDoc);
+            res.json(result);
+      })
 
 
 
